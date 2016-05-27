e59bb9fcf1c6d28acea190a148ac7e4b404e616b
Alexander Popov <alex.val.popov@gmail.com>
2016-05-27 09:56:18 +0300

    Add Note specs

---
+++ spec/models/note_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  subject { Fabricate(:note) }
+
+  it('has a valid fabricator') { expect(Fabricate(:note)).to be_valid }
+
+  context :associations do
+    it { should belong_to(:user) }
+  end
+
+  context :validations do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+end
