5f2ac7952a100589057b65d23a333c7b8378c4eb
Alexander Popov <alex.val.popov@gmail.com>
2016-05-29 17:03:33 +0300

    Add UsersController#create request specs

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'POST /users' do
+    context 'with valid parameters' do
+      let(:user_attributes) { Fabricate.attributes_for(:user) }
+
+      before(:each) { post_users(user_attributes) }
+
+      it 'responds with 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'matches the expected schema' do
+        expect(response).to match_response_schema('user')
+      end
+
+      it 'returns a user with correct attributes' do
+        user = JSON.parse(response.body, symbolize_names: true)[:user]
+        expect(user[:email]).to eq(user_attributes[:email])
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:user_attributes) { Fabricate.attributes_for(:user, email: nil) }
+
+      before(:each) do |example|
+        post_users(user_attributes) unless example.metadata[:skip_before]
+      end
+
+      it 'responds with 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'matches the expected schema' do
+        expect(response).to match_response_schema('errors')
+      end
+
+      it 'returns the validation errors', skip_before: true do
+        user = Fabricate.build(:user, user_attributes)
+        user.valid?
+        post_users(user_attributes)
+
+        expect(JSON.parse(response.body)['errors']).to match(user.errors.full_messages)
+      end
+    end
+  end
+end
+
+def post_users(params)
+  post '/users', {user: params}, format: :json
+end
