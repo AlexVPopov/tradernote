42937742cceb4351a109e00275cc40ef709ee9db
Alexander Popov <alex.val.popov@gmail.com>
2016-05-30 00:18:24 +0300

    Add route for Authentications#create

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
         constraints: APIVersion.new('v1', true),
         defaults: {format: :json} do
     resources :users, only: :create
+
+    resources :authentications, path: 'authenticate', only: :create
   end
 end
