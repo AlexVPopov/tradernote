8b4a4dd4c319be698481c38345eb49f74e1ebd1b
Alexander Popov <alex.val.popov@gmail.com>
2016-05-31 19:54:01 +0300

    Add specs for named scopes of Note

---
+++ spec/support/spec_helpers/notes_model_spec_helpers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+module NotesModelSpecHelpers
+  def title_matches(query, *results)
+    expect(Note.title_matches(query)).to match_array(results)
+  end
+
+  def body_matches(query, *results)
+    expect(Note.body_matches(query)).to match_array(results)
+  end
+
+  def matches(query, *results)
+    expect(Note.matches(query)).to match_array(results)
+  end
+end
